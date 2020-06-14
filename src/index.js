1b833174d2c7b1f172f1f4fafb19fcf3b42c8c69
Deohgu <Dioggopsanttos@gmail.com>
2020-06-14 22:54:10 +0100

    Changed enviroment with create-react-app

---
+++ src/index.js
@@ -0,0 +1,24 @@
+// const InputComponent = () => {
+//   <input type="text" tabindex="1">
+      
+//   </input>
+// };
+
+// // Rendering the input field
+// class InputField extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       category: ""
+//     };
+//   }
+  
+//   render() {
+//     <form>
+//       <inputComponent />
+      
+//     </form>
+//   }
+// }
+
+console.log('Hello World!');

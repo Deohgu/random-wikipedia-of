248c7bdf1cad7cf22600b2ac7a276519ffcfe452
Deohgu <dioggopsanttos@gmail.com>
2020-07-05 12:07:27 +0100

    Logs added easier visualization.

--- src/App.js
+++ src/App.js
@@ -12,14 +12,16 @@ const App = () => {
   const [ lockInputData, setLockInputData ] = useState("")
   const [prevLockInputData, setPrevLockInputData] = useState("")
   
-  console.log(`inputData = ${inputData}, lockInputData = ${lockInputData}, prevLockInputData = ${prevLockInputData}`);
+  console.log(`Character Inputed    inputData = ${inputData}, lockInputData = ${lockInputData}, prevLockInputData = ${prevLockInputData}`);
 
   // Currently requiring two presses to search, I wonder if the setLockInputData is stopping the thing from running by refreshing it somehow.
   // If you go through the steps carefully you can understand that the console.log() is running each time something is set. Have to ready about setState before continuing. 
   const submitData = async (dataToFetch) => {
+    console.log(`Button pressed, event handler run`)
     setLockInputData(dataToFetch); // same as inputData
 
     if (lockInputData !== "" && lockInputData !== prevLockInputData) {
+      console.log(`If statment inside button func ran`)
       await newCat(dataToFetch.replace(/[" "]/g, "_"));
       setPrevLockInputData(lockInputData);
     } else if (lockInputData !== "" && lockInputData === prevLockInputData) {

d39500561a31c254feedca41ab044d2cb30bc371
deohgu <dioggopsanttos@gmail.com>
2020-07-15 00:09:08 +0100

    Updates

---
+++ precache-manifest.31527a7af4470fb415e253311ff4aae6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b82c8e3ed179bf09cf4f3a6bafc4e7",
+    "url": "/random-wikipedia-of/index.html"
+  },
+  {
+    "revision": "94fef7ac51ae3209a269",
+    "url": "/random-wikipedia-of/static/css/main.3d62de8d.chunk.css"
+  },
+  {
+    "revision": "28940684c5cd2f063874",
+    "url": "/random-wikipedia-of/static/js/2.2883adff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-wikipedia-of/static/js/2.2883adff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94fef7ac51ae3209a269",
+    "url": "/random-wikipedia-of/static/js/main.5f37a297.chunk.js"
+  },
+  {
+    "revision": "58be65eb44eb835b144f",
+    "url": "/random-wikipedia-of/static/js/runtime-main.4b8e4a77.js"
+  }
+]);

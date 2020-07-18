864aeb41087b2cffd48a16cad76adf8c40fcd70a
deohgu <dioggopsanttos@gmail.com>
2020-07-18 19:39:05 +0100

    Cleaning and formatting.

--- src/App.js
+++ src/App.js
@@ -1,102 +1,85 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import { TitleInput } from './components';
-import styles from './App.module.css';
+import { TitleInput } from "./components";
+import styles from "./App.module.css";
 
-import { newCat, newSubCat, recommendedFunc } from './api';
+import { newCat, newSubCat, recommendedFunc } from "./api";
 
 const App = () => {
-
-  const [ inputData, setInputData ] = useState("");
-  const [ prevInputData, setPrevInputData ] = useState("");
-  const [ recommendedArr, setRecommendedArr ] = useState([]);
-  const [ recomPressed, setRecomPressed ] = useState(false);
-  const [ randomPage, setRandomPage ] = useState("Random");
+  const [inputData, setInputData] = useState("");
+  const [prevInputData, setPrevInputData] = useState("");
+  const [recommendedArr, setRecommendedArr] = useState([]);
+  const [recomPressed, setRecomPressed] = useState(false);
+  const [randomPage, setRandomPage] = useState("Random");
 
   // useEffect is returning an array of recommended searches based on input and in return we are rendering that array with .map to create several recommendations in the form of buttons.
   useEffect(() => {
     const fetchedData = async () => {
-    const dataTransf = await recommendedFunc(inputData);
-    setRecommendedArr(dataTransf);
-  };
+      const dataTransf = await recommendedFunc(inputData);
+      setRecommendedArr(dataTransf);
+    };
     fetchedData();
   }, [inputData]);
-  
-  useEffect( () => {
+
+  useEffect(() => {
     console.log(`inputData = ${inputData}, recomPressed = ${recomPressed}`);
     if (recomPressed === true) {
       submitData(inputData);
     }
     setRecomPressed(false);
-
   }, [recomPressed === true]);
 
   const submitData = async (dataToFetch) => {
     let fetchedData = "";
     if (dataToFetch !== "" && dataToFetch !== prevInputData) {
       fetchedData = await newCat(dataToFetch.replace(/[" "]/g, "_"));
-      console.log(`fetched data inside first if statment = ${fetchedData}`)
+      console.log(`fetched data inside first if statment = ${fetchedData}`);
       setPrevInputData(dataToFetch);
     } else if (dataToFetch !== "" && dataToFetch === prevInputData) {
       fetchedData = await newSubCat(dataToFetch.replace(/[" "]/g, "_"));
     }
     setRandomPage(fetchedData);
-  }
-
+  };
 
   return (
     <div className={styles.container}>
       <TitleInput
-        inputData={ inputData }
-        handleChange={ (e) => setInputData(e.target.value) } 
-        inputDataSubmit={ () => submitData(inputData) }
-        randomPageTitle={ randomPage }
+        inputData={inputData}
+        handleChange={(e) => setInputData(e.target.value)}
+        inputDataSubmit={() => submitData(inputData)}
+        randomPageTitle={randomPage}
       />
-      
-      
-      {/* CURRENTLY HERE! */}
-      
+
       {/* https://reactgo.com/react-focus-input/
       Need to set the focus after pressing the recommendations button, according to this I can invoke a function on the button click for it */}
-      
+
       {/* To improve I can remove the B-Class and Uppercase the word after. Check the recommendations with B inputData */}
-      
-      
+
       {/* https://stackoverflow.com/questions/54069253/usestate-set-method-not-reflecting-change-immediately
       According to this it means that states are only updated after each render, in this case because the function is within another function meaning a closure the state is still undefined. The fix is to always use useEffect for these fetches, only not when we need to get another random result. But maybe even then it would work with if statments on the useEffect, I just cannot be updating states there for some reason. */}
-      
-      <div className={styles.recContainer}> 
-        { /* ternary to not attempt to render the initial undefined value */
-        (recommendedArr !== undefined)
-          ? ( recommendedArr.map( (curr, index) => (
-              <button
-                key={index}
-                className={styles.recommendations}
-                type="button"
-                onClick={
-                  async () => {
-                    await setInputData( curr.replace(/Category:/g, ""));
+
+      <div className={styles.recContainer}>
+        {
+          /* ternary to not attempt to render the initial undefined value */
+          recommendedArr !== undefined
+            ? recommendedArr.map((curr, index) => (
+                <button
+                  key={index}
+                  className={styles.recommendations}
+                  type="button"
+                  onClick={async () => {
+                    setInputData(curr.replace(/Category:/g, ""));
                     setRecomPressed(true);
-                  }
-                }
-              >
-                {curr
-                  .replace(/Category:/g, "")
-                }
-              </button>
-            )) )
-          : null
+                  }}
+                >
+                  {curr.replace(/Category:/g, "")}
+                </button>
+              ))
+            : null
         }
       </div>
     </div>
   );
-}
-
-
-// App done whilst following a youtube video regarding a different app done with React
-// Left off at ####
-// https://www.youtube.com/watch?v=khJlrj3Y6Ls
-// Compare with this one to understand
-// https://www.youtube.com/watch?v=U9T6YkEDkMo
+};
 
 export default App;
--- src/api/index.js
+++ src/api/index.js
@@ -1,38 +1,29 @@
-// have it at 1 for level deep as standard.
-// Is it possible to have several levels deep without pulling several of them?Currently first fetch pulls pages and subcats. subsubcats would require 2 fetches for??
-
 // To do here:
 // If the picked subcat has no pages (probably only subcats) fetch a level deeper?  In total it's first Category Fetch, second SubCat fetch, and third random subCat fetch. For a new random it's simply 2 fetches. So not that many unless you're very unlucky.
-
-// Found an issue, the pages needs to reset after a new search is provided. This has likely seemed like a bug before when trying botany and then football 
-
 
 let url = "";
 let pages = [];
 let subCats = [];
 
-
 const fetchPush = async (category) => {
   const urlFetch = await fetch(url);
-  const { query: { categorymembers } } = await urlFetch.json();
-  
+  const {
+    query: { categorymembers },
+  } = await urlFetch.json();
+
   for await (const element of categorymembers) {
     if (element.type === "page") {
-      pages.push(element.title
-        .replace(/[" "]/g, "_"));
+      pages.push(element.title.replace(/[" "]/g, "_"));
     } else {
-      subCats.push(element.title
-        .replace(/Category:/g, "")
-        .replace(/[" "]/g, "_"));
+      subCats.push(
+        element.title.replace(/Category:/g, "").replace(/[" "]/g, "_")
+      );
     }
-  };
-  // Only to try and read elsewhere
-  // return categorymembers;
-}
+  }
+};
 
 // Called when a new category is entered
 export const newCat = async (category) => {
-
   // Resetting for the new category.
   pages = [];
   subCats = [];
@@ -44,7 +35,6 @@ export const newCat = async (category) => {
   await fetchPush(category);
 
   return newSubCat();
-  
 };
 
 // Called when a category is already entered but user wants another result
@@ -53,38 +43,30 @@ export const newSubCat = async () => {
   const randomSubCat = subCats[randomSubCatIndex];
   // Removing the subCat so that it won't be re-picked.
   subCats.splice(randomSubCatIndex, 1);
-  
+
   // Global category doesn't seem to work so I am changing the url here.
   url = `https://en.wikipedia.org/w/api.php?action=query&list=categorymembers&cmtitle=Category:${randomSubCat}&cmprop=title|type&format=json&cmlimit=500&cmtype=page&origin=*`;
 
   await fetchPush();
-  
-  
+
   // Random article in pages
   const randomPageIndex = Math.floor(Math.random() * pages.length);
   const randomPage = pages[randomPageIndex];
   // Removing the article page so that it won't be re-picked
   pages.splice(randomPageIndex, 1);
-  
-  
-  //Cannot get this to console.log, even though in fetchPush categorymembers in being returned, it simply says that it is not defined as if this console.log is running first. Maybe I still don't understand await. read the article in DEV.
-  // console.log(`categorymembers ${categorymembers}`);
+
   console.log(`Random Page = ${randomPage}`);
-  
-  return randomPage
-    .replace(/["_"]/g, " ");
+
+  return randomPage.replace(/["_"]/g, " ");
 };
 
-
-
-
 ///////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////
-
-
 
 export const recommendedFunc = async (data) => {
-  const recomendedFetch = await fetch(`https://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=6&namespace=14&suggest&search=${data}&origin=*`);
+  const recomendedFetch = await fetch(
+    `https://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=6&namespace=14&suggest&search=${data}&origin=*`
+  );
   const recommendedData = await recomendedFetch.json(); // Can't seem to destructure it.
   return recommendedData[1];
 };

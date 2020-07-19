931864c42f57a8531554d84fbb1f6a8a527fa9db
deohgu <dioggopsanttos@gmail.com>
2020-07-19 20:46:27 +0100

    Updates

---
+++ precache-manifest.5e6d7df3898c9b1ae3c64671047fdd1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c3fef8dd63e52dfafe3ba4eaac0bed",
+    "url": "/random-wikipedia-of/index.html"
+  },
+  {
+    "revision": "366cbfbbe6d0fda1bf70",
+    "url": "/random-wikipedia-of/static/css/main.53abaa7e.chunk.css"
+  },
+  {
+    "revision": "ed9cd10366eed6ed6ce9",
+    "url": "/random-wikipedia-of/static/js/2.9f7151d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-wikipedia-of/static/js/2.9f7151d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "366cbfbbe6d0fda1bf70",
+    "url": "/random-wikipedia-of/static/js/main.7e7d2b26.chunk.js"
+  },
+  {
+    "revision": "58be65eb44eb835b144f",
+    "url": "/random-wikipedia-of/static/js/runtime-main.4b8e4a77.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/random-wikipedia-of/precache-manifest.1a7b8932e804ca3b4085578017c65774.js"
+  "/random-wikipedia-of/precache-manifest.5e6d7df3898c9b1ae3c64671047fdd1e.js"
 );
 
 self.addEventListener('message', (event) => {

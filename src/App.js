e21662eaef1c06c8ff6c9a547b0778a82e75db69
Deohgu <dioggopsanttos@gmail.com>
2021-01-21 23:00:05 +0000

    🔥 🚧 🎨 A single function to handle fetch requests

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 
 import { TitleInput } from "./components";
 import styles from "./App.module.css";
@@ -9,25 +9,22 @@ const App = () => {
   const [inputData, setInputData] = useState("");
   const [prevInputData, setPrevInputData] = useState("");
   const [recommendedArr, setRecommendedArr] = useState([]);
-  const [recomPressed, setRecomPressed] = useState(false);
   const [randomPage, setRandomPage] = useState("Random");
 
   // useEffect is returning an array of recommended searches based on input and in return we are rendering that array with .map to create several recommendations in the form of buttons.
-  useEffect(() => {
-    const fetchedData = async () => {
-      const dataTransf = await recommendedFunc(inputData);
-      setRecommendedArr(dataTransf);
-    };
-    fetchedData();
-  }, [inputData]);
+  // useEffect(() => {
+  //   if (inputData !== "") {
+  //     const fetchedData = async () => {
+  //       const dataTransf = await recommendedFunc(inputData);
+  //       setRecommendedArr(dataTransf);
+  //     };
+  //     fetchedData();
+  //   }
+  // }, [inputData]);
 
-  useEffect(() => {
-    if (recomPressed === true) {
-      submitData(inputData);
-    }
-    setRecomPressed(false);
-  }, [recomPressed === true]);
+  // console.log(recommendedArr);
 
+  // CURENTLY HERE. Trying to figure how to call this by pressing the recommendations.
   const submitData = async (dataToFetch) => {
     let fetchedData = "";
     if (dataToFetch !== "" && dataToFetch !== prevInputData) {
@@ -39,34 +36,24 @@ const App = () => {
     setRandomPage(fetchedData);
   };
 
-  const searchInput = useRef(null);
-
-  const handleFocus = () => {
-    searchInput.current.focus();
+  const handleFetch = async (input) => {
+    const dataTransf = await (input && recommendedFunc(input)); // Fetches if input was passed
+    input ? setRecommendedArr(dataTransf) : setRecommendedArr([]); // Resets if no input === user deleted input fully
   };
 
   return (
     <div className={styles.container}>
       <TitleInput
         inputData={inputData}
-        handleChange={(e) => setInputData(e.target.value)}
+        handleInputData={(input) => setInputData(input)}
         inputDataSubmit={() => submitData(inputData)}
         randomPageTitle={randomPage}
         recommendedArr={recommendedArr}
         setInputData={setInputData}
-        setRecomPressed={setRecomPressed}
-        handleFocus={handleFocus}
-        searchInput={searchInput}
+        handleFetch={handleFetch}
       />
     </div>
   );
 };
 
 export default App;
-
-
-// Delete after
-// Done - Need to change the text to the planned one on evernote.
-// Done - Need to change the top margin when on the phone
-// Done - Need to change the bottom margin of the container where CATEGORY is, remove there and add to the recommendations container instead (mostly for the black bar at the moment)
-// Need to have a way to search the results without a subcategory,maybe for now use a secrect key. that calls a funtion with sub cats just to use on things like COnspiracy Theories.

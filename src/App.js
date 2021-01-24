c0b993c2540c74df4992e164e7f9bbff5eab11ac
Deohgu <dioggopsanttos@gmail.com>
2021-01-24 18:58:44 +0000

    🎨 Compatible with new API funcs

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import styles from "./App.module.css";
 
 import { recommendedFunc } from "./utils/recommendedFunc";
 import { newCat } from "./utils/newCat";
-import { newSubCat } from "./utils/newSubCat";
+import { anotherSubCat } from "./utils/anotherSubCat";
 
 // If an ERROR is received is it likely that it was done through form submit
 // See this issue https://github.com/Deohgu/random-wikipedia-of/issues/13
@@ -14,22 +14,27 @@ const App = () => {
   const [inputData, setInputData] = useState("");
   const [prevInputData, setPrevInputData] = useState("");
   const [recommendedArr, setRecommendedArr] = useState([]);
-  const [randomPage, setRandomPage] = useState("Random");
+  const [fetchedData, setFetchedData] = useState({
+    picked: "Random",
+    articles: [],
+    subCats: [],
+  });
 
   // Call API fetching funcs and handles the received data
-  // CURRENTLY HERE - newSubCat(dataToFetch) needs to receive the array
+  // CURRENTLY HERE - anotherSubCat(dataToFetch) needs to receive the array
   // Line 30. Because it selects a new category from the list, so it needs the list
+  // first if can be a string, but second must receive the initial data
   const submitData = async (dataToFetch) => {
-    let fetchedData = "";
+    let fetchedDataTemp = "";
     if (dataToFetch !== "" && dataToFetch !== prevInputData) {
       // fetches a new category with articles and subcategories
-      fetchedData = await newCat(dataToFetch);
+      fetchedDataTemp = await newCat(dataToFetch);
       setPrevInputData(dataToFetch);
     } else if (dataToFetch !== "" && dataToFetch === prevInputData) {
       // fetchs more articles from a another subcategory
-      fetchedData = await newSubCat(dataToFetch);
+      fetchedDataTemp = await anotherSubCat(fetchedData); // parameter expects old data to add more {picked: "", articles: [], subCats: []}
     }
-    setRandomPage(fetchedData);
+    setFetchedData(fetchedDataTemp);
   };
 
   // Data handler for new form inputs, buttons pressed, and form submit
@@ -45,7 +50,7 @@ const App = () => {
     <div className={styles.container}>
       <TitleInput
         inputData={inputData}
-        randomPage={randomPage}
+        fetchedData={fetchedData}
         recommendedArr={recommendedArr}
         fetchHandler={fetchHandler}
       />

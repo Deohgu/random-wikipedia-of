1800bfa01ce8e0ef67a95f3e368d3d602cd66947
Deohgu <dioggopsanttos@gmail.com>
2021-01-25 18:52:58 +0000

    🎨 Better naming

--- src/utils/newCat.js
+++ src/utils/newCat.js
@@ -1,19 +1,20 @@
-import { fetchPush } from './fetchPush'
+import { fetchList } from './fetchList'
 import { randomPicker } from './randomPicker'
 
 // Called when a new category is entered
 export const newCat = async (category) => {
   // fetches articles and subCats
   // articlesSubcats = { articles: [...], subCats: [...] }
-  const articlesSubcats = await fetchPush(category)
+  const articlesSubcats = await fetchList(category)
 
   // Can be improved by somewhat calling anotherSubCat
   // If depth of 2 ////////////////////////////
   const anotherCat = randomPicker(articlesSubcats, 'subCats') // a random category to add more data
 
-  articlesSubcats.articles = fetchPush(anotherCat).articles // adds more articles to original
-  articlesSubcats.subCats = fetchPush(anotherCat).subCats // adds more categories to original
-  /// /////////////////////////////////////////
+  const newData = fetchList(anotherCat)
+  articlesSubcats.articles.push(newData.articles) // adds more articles to original from picked category
+  articlesSubcats.subCats.push(newData.subCats) // adds more categories to original from picked category
+  /// //////////////////////////////////////////
 
   articlesSubcats.picked = randomPicker(articlesSubcats, 'article') // picks an article for the user
 

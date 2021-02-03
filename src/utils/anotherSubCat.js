73dab46b3efd562b98e4ea655d90ecb37ed5ba21
Deohgu <dioggopsanttos@gmail.com>
2021-02-03 18:26:52 +0000

    refactor: Proper promise handling

--- src/utils/anotherSubCat.js
+++ src/utils/anotherSubCat.js
@@ -6,12 +6,14 @@ export const anotherSubCat = async (fetchedData) => {
   const fetchedDataCopy = { ...fetchedData }
   const anotherCat = randomPicker(fetchedDataCopy, 'subCats') // a random category to add more data
 
-  const newData = fetchList(anotherCat)
-  fetchedDataCopy.articles.push(newData.articles) // adds more articles to original from picked category
-  fetchedDataCopy.subCats.push(newData.subCats) // adds more categories to original from picked category
+  const newData = await fetchList(anotherCat)
+    .then((parsedData) => parsedData)
+    .catch((error) => console.log(error))
+
+  fetchedDataCopy.articles.concat(newData.articles) // adds more articles to original from picked category
+  fetchedDataCopy.subCats.concat(newData.subCats) // adds more categories to original from picked category
 
   fetchedDataCopy.picked = randomPicker(fetchedDataCopy, 'article') // picks an article for the user
 
-  console.log(`anotherSubCat return list picked: ${fetchedDataCopy.picked}`)
   return fetchedDataCopy // {picked: "", articles: [], subCats: []}
 }

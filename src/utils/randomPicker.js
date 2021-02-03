6b1086589a83191ce121ad1d781cda57d6082fcf
Deohgu <dioggopsanttos@gmail.com>
2021-02-03 23:26:42 +0000

    Refactor: Removes comments

--- src/utils/randomPicker.js
+++ src/utils/randomPicker.js
@@ -5,18 +5,14 @@ export const randomPicker = (fetchedData, type) => {
       Math.random() * { ...fetchedData }.articles.length
     )
     const randomArticle = { ...fetchedData }.articles[randomPageIndex]
-    // Removing the article page so that it won't be re-picked
-    // {...fetchedData}.articles.splice(randomPageIndex, 1)
-    // return fetchedData;
+
     return randomArticle // Returns article to be used.
   } else if (type === 'subCats') {
     const randomSubCatIndex = Math.floor(
       Math.random() * { ...fetchedData }.subCats.length
     )
     const randomSubCat = { ...fetchedData }.subCats[randomSubCatIndex]
-    // Removing the subCat so that it won't be re-picked.
-    // {...fetchedData}.subCats.splice(randomSubCatIndex, 1) // WILL NOT DO ANYTHING LIKE THIS
-    // return fetchedData;
+
     return randomSubCat // Returns subCats to be used.
   }
 }

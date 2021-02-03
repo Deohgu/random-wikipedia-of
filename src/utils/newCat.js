ebc82c4ba06588eb46dca5a51d66cc3262850a79
Deohgu <dioggopsanttos@gmail.com>
2021-02-03 18:29:02 +0000

    clean: Removes notes and logs

--- src/utils/newCat.js
+++ src/utils/newCat.js
@@ -6,12 +6,17 @@ export const newCat = async (category) => {
   // fetches articles and subCats
   // articlesSubcats = { articles: [...], subCats: [...] }
   const articlesSubcats = await fetchList(category)
+    .then((parsedData) => parsedData)
+    .catch((error) => console.log(error))
 
   // Can be improved by somewhat calling anotherSubCat
   // If depth of 2 ////////////////////////////
   const anotherCat = randomPicker(articlesSubcats, 'subCats') // a random category to add more data
 
-  const newData = fetchList(anotherCat)
+  const newData = await fetchList(anotherCat)
+    .then((parsedData) => parsedData)
+    .catch((error) => error)
+
   articlesSubcats.articles.push(newData.articles) // adds more articles to original from picked category
   articlesSubcats.subCats.push(newData.subCats) // adds more categories to original from picked category
   /// //////////////////////////////////////////

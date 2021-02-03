7e3b0f0cc1b6faffeb514c7e91f7ee1c25567c80
Deohgu <dioggopsanttos@gmail.com>
2021-02-03 18:28:22 +0000

    refactor: Proper promise handling

--- src/utils/fetchList.js
+++ src/utils/fetchList.js
@@ -4,19 +4,25 @@ export const fetchList = async (category) => {
     '_'
   )}&cmprop=title|type&format=json&cmlimit=500&cmtype=page|subcat&origin=*` // cannot lower case URLs, Wikipedia categories are case sensitive
   const fetchedData = { articles: [], subCats: [] }
-  const urlFetch = await fetch(url)
-  const {
-    query: { categorymembers }
-  } = await urlFetch.json()
+  const response = await fetch(url)
 
-  for (const element of categorymembers) {
-    if (element.type === 'page') {
-      fetchedData.articles.push(element.title.replace(/[" "]/g, '_')) // articles/pages to pages array
-    } else {
-      fetchedData.subCats.push(
-        element.title.replace(/Category:/g, '').replace(/[" "]/g, '_') // categories of main category/subCats to subCats array
-      )
+  if (!response.ok) { // promise returns error
+    const message = `An error has occured: ${response.status}`
+    throw new Error(message)
+  } else {
+    const {
+      query: { categorymembers }
+    } = await response.json()
+
+    for (const element of categorymembers) {
+      if (element.type === 'page') {
+        fetchedData.articles.push(element.title.replace(/[" "]/g, '_')) // articles/pages to pages array
+      } else {
+        fetchedData.subCats.push(
+          element.title.replace(/Category:/g, '').replace(/[" "]/g, '_') // categories of main category/subCats to subCats array
+        )
+      }
     }
+    return fetchedData
   }
-  return fetchedData
 }

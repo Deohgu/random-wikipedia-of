f2ad8a82bd32bdddb1b106977cc375d84924c145
Deohgu <dioggopsanttos@gmail.com>
2021-02-03 23:34:49 +0000

    Fix: Properly concats old and new array

--- src/utils/anotherSubCat.js
+++ src/utils/anotherSubCat.js
@@ -6,14 +6,22 @@ export const anotherSubCat = async (fetchedData) => {
   const fetchedDataCopy = { ...fetchedData }
   const anotherCat = randomPicker(fetchedDataCopy, 'subCats') // a random category to add more data
 
+  // Removing the category so that it won't be re-picked
+  const indexOfanotherCat = fetchedDataCopy.subCats.indexOf(anotherCat);
+  fetchedDataCopy.subCats.splice(indexOfanotherCat, 1);
+
   const newData = await fetchList(anotherCat)
     .then((parsedData) => parsedData)
     .catch((error) => console.log(error))
 
-  fetchedDataCopy.articles.concat(newData.articles) // adds more articles to original from picked category
-  fetchedDataCopy.subCats.concat(newData.subCats) // adds more categories to original from picked category
+    fetchedDataCopy.articles = fetchedDataCopy.articles.concat(newData.articles) // adds more articles to original from picked category
+    fetchedDataCopy.subCats = fetchedDataCopy.subCats.concat(newData.subCats) // adds more categories to original from picked category
 
   fetchedDataCopy.picked = randomPicker(fetchedDataCopy, 'article') // picks an article for the user
+
+  // Removing the article so that it won't be re-picked
+  const indexOfPicked = fetchedDataCopy.articles.indexOf(fetchedDataCopy.picked);
+  fetchedDataCopy.articles.splice(indexOfPicked, 1);
 
   return fetchedDataCopy // {picked: "", articles: [], subCats: []}
 }
--- src/utils/newCat.js
+++ src/utils/newCat.js
@@ -13,15 +13,23 @@ export const newCat = async (category) => {
   // If depth of 2 ////////////////////////////
   const anotherCat = randomPicker(articlesSubcats, 'subCats') // a random category to add more data
 
+    // Removing the category so that it won't be re-picked
+    const indexOfanotherCat = articlesSubcats.subCats.indexOf(anotherCat);
+    articlesSubcats.subCats.splice(indexOfanotherCat, 1);
+
   const newData = await fetchList(anotherCat)
     .then((parsedData) => parsedData)
     .catch((error) => error)
 
-  articlesSubcats.articles.push(newData.articles) // adds more articles to original from picked category
-  articlesSubcats.subCats.push(newData.subCats) // adds more categories to original from picked category
+    articlesSubcats.articles = articlesSubcats.articles.concat(newData.articles) // adds more articles to original from picked category
+    articlesSubcats.subCats = articlesSubcats.subCats.concat(newData.subCats) // adds more categories to original from picked category
   /// //////////////////////////////////////////
 
   articlesSubcats.picked = randomPicker(articlesSubcats, 'article') // picks an article for the user
+
+    // Removing the article so that it won't be re-picked
+    const indexOfPicked = articlesSubcats.articles.indexOf(articlesSubcats.picked);
+    articlesSubcats.articles.splice(indexOfPicked, 1);
 
   return articlesSubcats // {picked: "", articles: [], subCats: []}
 }

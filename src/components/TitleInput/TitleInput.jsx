5731c443e908d596e8529d26c159fc6b3e824329
Deohgu <dioggopsanttos@gmail.com>
2021-02-04 11:28:53 +0000

    Refactor: Improves readability

--- src/components/TitleInput/TitleInput.jsx
+++ src/components/TitleInput/TitleInput.jsx
@@ -1,32 +1,38 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import styles from './TitleInput.module.css'
 
-const TitleInput = ({
+export const TitleInput = ({
   inputData,
   fetchedData,
-  recommendedArr,
-  fetchHandler
+  fetchHandler,
+  inputFocus
 }) => {
-  const inputFocus = useRef(null)
-
-  // Enables me to use multiple elements in the ternary operator.
-  // To be improved.
-  const componentToRenderTwo = (
-    <>
-      YOU HAVE THE linked{' '}
-      <a
-        href={'https://en.wikipedia.org/wiki/' + fetchedData.picked}
-        className={styles.randomPageTitle}
-      >
-        {fetchedData.picked + ' '}
-      </a>
-    </>
-  )
+  // Content before and after category is picked
+  const toDisplayConditionally = () => {
+    if (fetchedData.picked === 'Random') {
+      return (
+        <h1 className={styles.title}>
+          YOU WILL BE PROVIDED A RANDOM ARTICLE.
+        </h1>
+      )
+    } else {
+      return (
+        <h1 className={styles.title}>
+          YOU HAVE THE linked{' '}
+          <a
+            href={'https://en.wikipedia.org/wiki/' + fetchedData.picked.replace(/[" "]/g, '_')}
+            className={styles.randomPageTitle}
+          >
+            {fetchedData.picked}
+          </a>
+          {' '}ARTICLE.
+        </h1>
+      )
+    }
+  }
 
   return (
     <div className={styles.container}>
-      {/* Link needs to be more obvious. */}
-
       <h1 className={styles.title}>WITHIN THE WIKIPEDIA CATEGORY OF</h1>
       <form
         onSubmit={(e) => {
@@ -40,37 +46,13 @@ const TitleInput = ({
           placeholder=''
           value={inputData}
           onChange={(e) => {
-            fetchHandler(e.target.value, false) // false === fetchHandler does not call submitData
-          }} // Calls recommendedFunc to fetch recommendations when a new character is inserted
+            fetchHandler(e.target.value, false) // fetchHandler(..., false) runs but does not call submitData
+          }} // fetches recommendations when a new character is inserted
           autoFocus
           ref={inputFocus}
         />
       </form>
-      <h1 className={styles.title}>
-        {fetchedData === 'Random'
-          ? `YOU WILL BE PROVIDED A ${fetchedData} `
-          : componentToRenderTwo}
-        ARTICLE.
-      </h1>
-
-      <div className={styles.recContainer}>
-        {recommendedArr.length > 0 &&
-          recommendedArr.map((curr, index) => (
-            <button
-              key={`recommended${index}`}
-              className={styles.recommendations}
-              type='button'
-              onClick={async () => {
-                fetchHandler(curr, true)
-                inputFocus.current.focus()
-              }}
-            >
-              {curr}
-            </button>
-          ))}
-      </div>
+      {toDisplayConditionally()}
     </div>
   )
 }
-
-export default TitleInput

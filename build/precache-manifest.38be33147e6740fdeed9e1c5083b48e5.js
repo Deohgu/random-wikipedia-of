f66f34259d7b84a4fd8c1494d351b88b70334951
Deohgu <dioggopsanttos@gmail.com>
2021-02-08 13:56:12 +0000

    Build

---
+++ build/precache-manifest.38be33147e6740fdeed9e1c5083b48e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6af6735a35cf59d9cd7c3b256efa4ace",
+    "url": "/index.html"
+  },
+  {
+    "revision": "340e74d5604ebdb2c4c8",
+    "url": "/static/js/2.b4a052aa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b4a052aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7287d32787e8519f5132",
+    "url": "/static/js/main.c8eee4d1.chunk.js"
+  },
+  {
+    "revision": "47dcec91d52cd0f45f41",
+    "url": "/static/js/runtime-main.1577c24c.js"
+  }
+]);
